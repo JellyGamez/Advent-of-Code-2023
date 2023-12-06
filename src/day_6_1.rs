2b99df0af73242e2eb55e7a458d466c5c67a13e6
JellyGamez <alexcroitoriu2005@gmail.com>
2023-12-06 17:08:42 +0200

    day 6

--- src/day_6_1.rs
+++ src/day_6_1.rs
@@ -3,11 +3,23 @@ use std::io::{BufRead, BufReader};
 
 #[allow(dead_code)]
 pub fn main() {
-    let mut ans = 0;
+    let mut ans = 1;
     if let Ok(file) = File::open("input.txt") {
         let lines: Vec<String> = BufReader::new(file).lines().map(|l| l.unwrap()).collect();
-        for line in lines {
-
+        let times: Vec<i64> = lines[0].clone().split(':').nth(1).unwrap().split_whitespace().map(|s| s.parse::<i64>().unwrap()).collect();
+        let distances: Vec<i64> = lines[1].clone().split(':').nth(1).unwrap().split_whitespace().map(|s| s.parse::<i64>().unwrap()).collect();
+        for i in 0..times.len() {
+            let time = times[i];
+            let distance = distances[i];
+            let mut cur = 0;
+            for j in 1..=time / 2 + 1 {
+                if j * (time - j) > distance {
+                    cur = time + 1 - 2 * j;
+                    break;
+                }
+            }
+            println!("{cur}");
+            ans *= cur;
         }
     }
     print!("{ans}");
--- src/day_6_2.rs
+++ src/day_6_2.rs
@@ -3,11 +3,21 @@ use std::io::{BufRead, BufReader};
 
 #[allow(dead_code)]
 pub fn main() {
-    let mut ans = 0;
+    let mut ans = 1;
     if let Ok(file) = File::open("input.txt") {
         let lines: Vec<String> = BufReader::new(file).lines().map(|l| l.unwrap()).collect();
-        for line in lines {
+        let times: Vec<&str> = lines[0].split(':').nth(1).unwrap().split_whitespace().collect();
+        let time: i128 = times.join("").parse::<i128>().unwrap();
 
+        let distances: Vec<&str> = lines[1].split(':').nth(1).unwrap().split_whitespace().collect();
+        let distance: i128 = distances.join("").parse::<i128>().unwrap();
+
+        println!("{} {}", time, distance);
+        for j in 1..=time / 2 + 1 {
+            if j * (time - j) > distance {
+                ans = time + 1 - 2 * j;
+                break;
+            }
         }
     }
     print!("{ans}");
